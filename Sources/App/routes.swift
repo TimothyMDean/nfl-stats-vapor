6001d5cc37b667a0c2ca3839f8ed29542aecaf4f
Timothy Dean <tdean@Timothys-MacBook-Pro.local>
2018-12-25 21:36:14 -0600

    Remove default `Todo` classes and configurations

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,12 +3,6 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
-
     // Routes for managing NFL conferences
     let conferenceController = ConferenceController()
     router.get("conferences", use: conferenceController.index)
