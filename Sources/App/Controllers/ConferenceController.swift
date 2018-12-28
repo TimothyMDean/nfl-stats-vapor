5cc64f4534cef28b8bc013365a0a4d1775cf7ab5
Timothy Dean <tdean@Timothys-MacBook-Pro.local>
2018-12-27 23:36:14 -0600

    Moved rout definitions for `ConferenceController`

--- Sources/App/Controllers/ConferenceController.swift
+++ Sources/App/Controllers/ConferenceController.swift
@@ -1,7 +1,15 @@
 import Vapor
 
 /// Controls basic CRUD operations on `Conference`s.
-final class ConferenceController {
+final class ConferenceController: RouteCollection {
+
+    /// Registers this controller's routes at boot time
+    func boot(router: Router) throws {
+      let conferencesRoute = router.grouped("conferences")
+      conferencesRoute.get(use: index)
+      conferencesRoute.post(Conference.self, use: create)
+      conferencesRoute.delete(Conference.parameter, use: delete)
+    }
 
     /// Returns a list of all `Conference`s.
     func index(_ req: Request) throws -> Future<[Conference]> {
@@ -9,10 +17,8 @@ final class ConferenceController {
     }
 
     /// Saves a decoded `Conference` to the database.
-    func create(_ req: Request) throws -> Future<Conference> {
-        return try req.content.decode(Conference.self).flatMap { conference in
-            return conference.save(on: req)
-        }
+    func create(_ req: Request, conference: Conference) throws -> Future<Conference> {
+      return conference.save(on: req)
     }
 
     /// Deletes a parameterized `Conference`.
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,9 +3,5 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
 
-    // Routes for managing NFL conferences
-    let conferenceController = ConferenceController()
-    router.get("conferences", use: conferenceController.index)
-    router.post("conferences", use: conferenceController.create)
-    router.delete("conferences", Conference.parameter, use: conferenceController.delete)
+    try router.register(collection: ConferenceController())
 }
