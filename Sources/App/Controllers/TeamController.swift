f100bb7136dafd1a856569473352764e61eee1e0
Timothy Dean <tdean@Timothys-MacBook-Pro.local>
2018-12-28 22:01:11 -0600

    Added route to get conference for a team

--- Sources/App/Controllers/TeamController.swift
+++ Sources/App/Controllers/TeamController.swift
@@ -9,6 +9,7 @@ final class TeamController: RouteCollection {
     teamsRoute.get(use: index)
     teamsRoute.get(Team.parameter, use: get)
     teamsRoute.get(Team.parameter, "division", use: getDivision)
+    teamsRoute.get(Team.parameter, "conference", use: getConference)
   }
 
   /// Returns a list of all `Team`s.
@@ -25,6 +26,15 @@ final class TeamController: RouteCollection {
   func getDivision(_ req: Request) throws -> Future<Division> {
     return try req.parameters.next(Team.self).flatMap { team in
       team.division.get(on: req)
+    }
+  }
+
+  /// Returns the `Conference` for a `Team`
+  func getConference(_ req: Request) throws -> Future<Conference> {
+    return try req.parameters.next(Team.self).flatMap { team in
+      return team.division.get(on: req).flatMap { division in
+        return division.conference.get(on: req)
+      }
     }
   }
 }
