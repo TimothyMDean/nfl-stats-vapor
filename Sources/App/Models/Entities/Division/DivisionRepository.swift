0731043c170f9ac52eca18b38b45c0969467a384
Timothy Dean <tdean@Timothys-MacBook-Pro.local>
2019-01-20 23:08:41 -0600

    Correct comment format

--- Sources/App/Models/Entities/Division/DivisionRepository.swift
+++ Sources/App/Models/Entities/Division/DivisionRepository.swift
@@ -1,14 +1,14 @@
 import Vapor
 
-// A protocol that defines the interface to a `Division` entity repository.
+/// A protocol that defines the interface to a `Division` entity repository.
 protocol DivisionRepository: ServiceType {
 
-  // Retrieves a `Division` with a specified ID
+  /// Retrieves a `Division` with a specified ID
   func find(id: UUID) -> Future<Division?>
 
-  // Retrieves all `Division` entites for a conference ID
+  /// Retrieves all `Division` entities for a conference ID
   func find(conferenceId: UUID) -> Future<[Division]>
 
-  // Retrieves all `Division` entities
+  /// Retrieves all `Division` entities
   func all() -> Future<[Division]>
 }
