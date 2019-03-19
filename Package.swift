b3005481772dceb7d69d8edb21810929e566e0e8
Timothy Dean <tdean@Timothys-MacBook-Pro.local>
2019-03-19 09:25:03 -0500

    Style cleanup - hyphenation in package file

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "nfl-stats-vapor",
+    name: "nfl\u{2D}stats\u{2D}vapor",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent\u{2D}sqlite.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
